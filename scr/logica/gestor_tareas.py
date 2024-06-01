9b12246e627c8382c7e7407a0fe625ec69f5f683
AndreeZL <143139351+AndreeZL@users.noreply.github.com>
2024-05-31 19:52:54 -0500

    Lanzamiento listo

--- scr/logica/gestor_tareas.py
+++ scr/logica/gestor_tareas.py
@@ -12,3 +12,14 @@ class GestorTareas:
             raise ValueError("El título no puede estar vacío")
         tarea = Tarea(titulo, descripcion)
         self.tareas.append(tarea)
+
+    def test_marcar_completada(self):
+        self.gestor.agregar_tarea("Tarea 1", "Descripción de la tarea 1")
+        self.gestor.marcar_completada(0)
+        self.assertTrue(self.gestor.tareas[0].completada)
+
+    def eliminar_tarea(self, indice):
+        if 0 <= indice < len(self.tareas):
+            del self.tareas[indice]
+        else:
+            raise IndexError("Índice fuera de rango")
